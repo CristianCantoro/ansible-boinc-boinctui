aa88a23a2b5f3e4d9e818e054b892efc566b01a6
Cristian Consonni <cristian@balist.es>
2018-08-03 23:42:13 +0200

    Update deploy script

--- deploy_boinc_server.sh
+++ deploy_boinc_server.sh
@@ -8,14 +8,63 @@ if ! $SOURCED; then
   IFS=$'\n\t'
 fi
 
-if [ "$#" -lt 1 ]; then
-  (>&2 echo "Error. Missing argument.")
-  (>&2 echo "Usage:")
-  (>&2 echo "deploy_boinc_server.sh <ssh_port>")
+usage() {
+  echo \
+"Usage:
+  deploy_boinc_server.sh [options] [<ssh_port>]
+
+Deploy a BOINC server.
+
+Options:
+  -h              Show this help and exits.
+  -s <ssh_port>   Value of the option ssh_server_ports
+
+Example:
+  deploy_boinc_server.sh"
+}
+
+flag_help=false
+ssh_port='0'
+while getopts ":hs:" opt; do
+  case $opt in
+    h)
+      flag_help=true
+      ;;
+    s)
+      ssh_port="$OPTARG"
+
+      int_regex='^[0-9]+$'
+      if ! [[ "$ssh_port" =~ $int_regex ]] ; then
+        (>&2 echo "Error: option -s needs a number, got $ssh_port instead.")
+        exit 1
+      elif ! [[ "$ssh_port" -gt 0  && "$ssh_port" -le 65536 ]]; then
+        (>&2 echo "Error: option -s is for an ssh_port 0 < ssh_port < 65536.")
+        exit 1
+      fi
+      ;;
+    \?)
+      (>&2 echo "Invalid option: -$OPTARG")
+      ;;
+    :)
+      (>&2 echo "Option -$OPTARG requires an argument.")
+      exit 1
+      ;;
+  esac
+done
+
+if $flag_help; then
+  usage
+  exit 0
 fi
 
+options=()
+if [[ "$ssh_port" -gt 0 ]]; then
+  options=(--extra-vars "{'ssh_server_ports': [$ssh_port]}")
+fi
+
+set -x
+# shellcheck disable=SC2086
 ansible-playbook -v \
-                 -i inventories/production \
-                 --ask-vault-pass -e@inventories/production/group_vars/shadowsocks-servers.vault.yml \
-                 --extra-vars "ssh_server_ports=[$1]" \
-                    install_shadowsocks.yml 
+                 -i inventories/production/hosts \
+                 ${options[*]:-} \
+                    install_boinc.yml
